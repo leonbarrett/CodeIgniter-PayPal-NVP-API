93a3d1d4c4b304341ae157e18dfbf75d9f86bb54
Leon Barrett <leon@leonbarrett.com>
2010-04-09 21:57:58 +0000

    Updated the welcome_message.php view

--- system/application/views/welcome_message.php
+++ system/application/views/welcome_message.php
@@ -54,27 +54,25 @@ code {
 
 <h1>User Guide</h1>
 
-<p>This library has been provided by Leon Barrett (<a href="http://www.twitter.com/leonbarrett">@leonbarrett</a>) to provide a simple and clean way to interact with the PayPal NVP API. It builds upon concepts from the Code Igniter PayPal Library (v. 0.1) by <a href="http://aroussi.com/ci/paypal_lib/">http://aroussi.com/ci/paypal_lib</a>.</p>
+<p>This library has been provided by Leon Barrett (<a href="http://www.twitter.com/leonbarrett" target="_blank">@leonbarrett</a>) to provide a simple and clean way to interact with the PayPal NVP API. It builds upon concepts from the Code Igniter PayPal Library (v. 0.1) by <a href="http://aroussi.com/ci/paypal_lib" target="_blank">http://aroussi.com/ci/paypal_lib</a>.</p>
 
 <p>This library assumes that you have some prior knowledge of the PayPal API. More information can be found on the <a href="https://cms.paypal.com/us/cgi-bin/?cmd=_render-content&content_ID=developer/howto_api_reference" target="_blank">PayPal API reference pages</a>.</p>
 
+<p>It should be noted that this library isn't the magic answer to your PayPal solution, you will need to be comfortable with PHP, CI and the PayPal API!</p>
 <p>This package consists of the following files</p>
 
 <ul>
-	<li>Config > autoload.php - Use to auto load the paypal_lib.php and my paypal_api_lib.php libraries, the paypal_helper.php helper and the paypallib_config config file.</li>
-	<li>Controllers > demo.php used to demonstrate using the library to display the balance of the current PayPal account.</li>
+	<li>Config > autoload.php - used to auto load the libraries and the helper | paypallib_config - used to hold your API credentails.</li>
+	<li>Controllers > demo.php - used to demonstrate using the library to display the balance of the current PayPal account.</li>
+	<li>Helpers > paypal_helper.php - used to display any errors returned from the API call.</li>
+	<li>Libraries > paypal_api_lib.php - used to interact with the API, make a call and get a response | paypal_lib.php - used to receive IPN notifications from PayPal.</li>
 </ul>
 
-<p>If you would like to edit this page you'll find it located at:</p>
-<code>system/application/views/welcome_message.php</code>
+<p><strong>Please note the library requires <a href="http://codeigniter.com/" target="_blank">CI 1.7.2</a> and higher</strong></p>
 
-<p>The corresponding controller for this page is found at:</p>
-<code>system/application/controllers/welcome.php</code>
+<h2>Documentation</h2>
 
-<p>If you are exploring CodeIgniter for the very first time, you should start by reading the <a href="user_guide/">User Guide</a>.</p>
-
-
-<p><br />Page rendered in {elapsed_time} seconds</p>
+<p>Full documentation can be found online at the following location - <a href="http://www.leonbarrett.com/code/paypal/">http://www.leonbarrett.com/code/paypal/</a>.</p>
 
 </body>
 </html>
